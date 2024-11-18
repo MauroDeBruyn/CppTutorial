30b8f58d892ae99950d478e536758e98f6983fda
Mauro <maurodebruyn@hotmail.com>
2024-11-18 22:11:19 +0100

    Add files

---
+++ Files/build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_analogclock_FILE C:/Qt/Examples/Qt-6.7.2/widgets/widgets/analogclock/build/Desktop-Debug/analogclock.exe)
+set(__QT_DEPLOY_TARGET_analogclock_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_analogclock_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
---
+++ Files/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/deploy_analogclock_0c79c825b7.cmake
@@ -0,0 +1,8 @@
+include(C:/Qt/Examples/Qt-6.7.2/widgets/widgets/analogclock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/analogclock-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_ALL_MODULES_FOUND_VIA_FIND_PACKAGE "ZlibPrivate;EntryPointPrivate;Core;Gui;Widgets")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE C:/Qt/Examples/Qt-6.7.2/widgets/widgets/analogclock/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/analogclock.exe
+    GENERATE_QT_CONF
+)
