134867df4f9973feaf76098101ec884ecfaccbff
Mauro <maurodebruyn@hotmail.com>
2024-12-10 16:30:32 +0100

    //

--- Files/analogclock.h
+++ Files/analogclock.h
@@ -1,12 +1,8 @@
-// Copyright (C) 2016 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
-
 #ifndef ANALOGCLOCK_H
 #define ANALOGCLOCK_H
 
 #include <QWidget>
 
-//! [0]
 class AnalogClock : public QWidget
 {
     Q_OBJECT
@@ -17,6 +13,5 @@ public:
 protected:
     void paintEvent(QPaintEvent *event) override;
 };
-//! [0]
 
 #endif
